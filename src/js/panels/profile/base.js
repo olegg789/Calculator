f1d0795054428a9145df0b94f4910814731d49a5
olegg789 <olegchikelev789789@gmail.com>
2022-01-16 14:29:53 +0300

    fixes

--- src/js/panels/profile/base.js
+++ src/js/panels/profile/base.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
     Group,
@@ -16,8 +16,29 @@ import {evaluate} from "mathjs";
 
 function ProfilePanelBase({isDesktop, router}) {
 
+    const [setTo] = useState(null)
+    const [disabled, setDisabled] = useState(true)
+    const [start, setStart] = useState(null);
     const textInput = React.createRef();
     const clear = () => (textInput.current.value = "");
+
+    async function onChange() {
+        try {
+            let data = document.getElementById('start')
+            await setStart(data.value)
+        }
+        catch(err) {
+            console.log(err)
+        }
+
+    }
+
+    async function unDisabledButton() {
+        let data = document.getElementById('to');
+        await setTo(data.value);
+        await 1;
+        setDisabled(false)
+    }
 
     function calc() {
         try {
@@ -25,9 +46,11 @@ function ProfilePanelBase({isDesktop, router}) {
             let num = document.getElementById('num').value;
             let to = document.getElementById('to').value;
 
-            let test1 = evaluate(num + ' ' + from + ' to ' + to)
-            document.getElementById('result').innerHTML = test1
+            let result = evaluate(num + ' ' + from + ' to ' + to);
+
+            document.getElementById('result').innerHTML = result;
         }
+
         catch(err) {
             document.getElementById('result').innerHTML = 'ERROR'
         }
@@ -38,14 +61,95 @@ function ProfilePanelBase({isDesktop, router}) {
             <PanelHeader separator={false}>Конвертор</PanelHeader>
             <Div>Позволит перевести из одной единицы измерения в другую</Div>
                 <Group mode='card'>
+                    <FormItem top='Физическая величина'>
+                        <NativeSelect id='start' placeholder="Не выбрана" onChange={() => onChange()}>
+                            <option value='dlina'>Длина</option>
+                            <option value='massa'>Масса</option>
+                            <option value='vremya'>Время</option>
+                        </NativeSelect>
+                    </FormItem>
+                    {start === 'dlina' &&
+                        <>
                         <FormItem top="Из...">
                             <NativeSelect id='from' placeholder='Не выбрано'>
                                 <option value="cm">Сантиметры</option>
                                 <option value="m">Метры</option>
                                 <option value="km">Километры</option>
+                            </NativeSelect>
+                        </FormItem>
+                        <Div>
+                            <Input
+                                getRef={textInput}
+                                type="number"
+                                id='num'
+                                placeholder="Введите число..."
+                                after={
+                                    <IconButton
+                                        hoverMode="opacity"
+                                        aria-label="Очистить поле"
+                                        onClick={clear}
+                                    >
+                                        <Icon16Clear />
+                                    </IconButton>
+                                }
+                            />
+                        </Div>
+                        <Div></Div>
+                        <FormItem top="В...">
+                            <NativeSelect id='to' placeholder='Не выбрано' onChange={() => unDisabledButton()}>
+                                <option value="cm">Сантиметры</option>
+                                <option value="m">Метры</option>
+                                <option value="km">Километры</option>
+                            </NativeSelect>
+                        </FormItem>
+                        <Div>
+                            <Button size='l' disabled={disabled} stretched onClick={() => calc()}>Вычислить</Button>
+                        </Div>
+                        </>}
+                    {start === 'massa' &&
+                    <>
+                        <FormItem top="Из...">
+                            <NativeSelect id='from' placeholder='Не выбрано'>
                                 <option value='g'>Граммы</option>
                                 <option value="kg">Килограммы</option>
                                 <option value="ton">Тонны</option>
+                            </NativeSelect>
+                        </FormItem>
+                        <Div>
+                            <Input
+                                getRef={textInput}
+                                type="number"
+                                id='num'
+                                placeholder="Введите число..."
+                                after={
+                                    <IconButton
+                                        hoverMode="opacity"
+                                        aria-label="Очистить поле"
+                                        onClick={clear}
+                                    >
+                                        <Icon16Clear />
+                                    </IconButton>
+                                }
+                            />
+                        </Div>
+                        <Div></Div>
+                        <FormItem top="В...">
+                            <NativeSelect id='to' placeholder='Не выбрано' onChange={() => unDisabledButton()}>
+                                <option value='g'>Граммы</option>
+                                <option value="kg">Килограммы</option>
+                                <option value="ton">Тонны</option>
+                            </NativeSelect>
+                        </FormItem>
+                        <Div>
+                            <Button size='l' disabled={disabled} stretched onClick={() => calc()}>Вычислить</Button>
+                        </Div>
+                    </>
+
+                    }
+                    {start === 'vremya' &&
+                    <>
+                        <FormItem top="Из...">
+                            <NativeSelect id='from' placeholder='Не выбрано'>
                                 <option value="s">Секунды</option>
                                 <option value='minute'>Минуты</option>
                                 <option value='hour'>Часы</option>
@@ -71,13 +175,7 @@ function ProfilePanelBase({isDesktop, router}) {
                         </Div>
                         <Div></Div>
                         <FormItem top="В...">
-                            <NativeSelect id='to' placeholder='Не выбрано'>
-                                <option value="cm">Сантиметры</option>
-                                <option value="m">Метры</option>
-                                <option value="km">Километры</option>
-                                <option value='g'>Граммы</option>
-                                <option value="kg">Килограммы</option>
-                                <option value="ton">Тонны</option>
+                            <NativeSelect id='to' placeholder='Не выбрано' onChange={() => unDisabledButton()}>
                                 <option value="s">Секунды</option>
                                 <option value='minute'>Минуты</option>
                                 <option value='hour'>Часы</option>
@@ -85,8 +183,10 @@ function ProfilePanelBase({isDesktop, router}) {
                             </NativeSelect>
                         </FormItem>
                         <Div>
-                            <Button size='l' stretched onClick={() => calc()}>Вычислить</Button>
+                            <Button size='l' disabled={disabled} stretched onClick={() => calc()}>Вычислить</Button>
                         </Div>
+                    </>
+                    }
                 </Group>
                 <Group header={<Header>Результат</Header>}>
                     <Div>
